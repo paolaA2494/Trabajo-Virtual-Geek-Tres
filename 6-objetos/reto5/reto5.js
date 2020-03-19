32e9066d800387e0a9db52ad28f1f50dded01037
Casta <palolahrn0104@gmail.com>
2020-03-19 00:38:23 -0500

    Primera información

---
+++ 6-objetos/reto5/reto5.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+const product = (a, b) => {
+    const result = a * b;
+    return result;
+}
+
+console.log('El producto entre los dos datos ingresados es: ' +product(1, 4) );
+console.log('El producto entre los dos datos ingresados es: ' +product(2, 10) );
+console.log('El producto entre los dos datos ingresados es: ' +product(11, 3) );
